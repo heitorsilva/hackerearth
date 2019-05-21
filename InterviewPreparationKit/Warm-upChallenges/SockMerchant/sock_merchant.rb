cf6c4bd036c67569aab6748c46bb8e3977bd30f2
Heitor Silva <heitor.silva@aklamio.com>
2019-05-21 16:39:31 +0200

    Ruby solution to 'Sock Merchant'

---
+++ InterviewPreparationKit/Warm-upChallenges/SockMerchant/sock_merchant.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'stringio'
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar)
+ar.group_by{|i| i}.map{|k,v| [k, v.count] }.reduce(0){|sum, sock| sock[1] % 2 == 0 ? sum + sock[1] / 2 : sum + (sock[1]-1) / 2 }
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+n = gets.to_i
+
+ar = gets.rstrip.split(' ').map(&:to_i)
+
+result = sockMerchant n, ar
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
